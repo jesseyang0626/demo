2beb1dabb8ed4947cf660ea5a6e54d37a17ae66e
Administrator <Administrator@USER-20170124KP>
2017-05-03 21:56:39 +0800

    1

---
+++ src/main/java/com/dz/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.dz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping("/")
+	public String helloWorld(){
+		return "helloWorld";
+	}
+}
